42ffe1dddf11e4988e3e5cc87cb9d23e0a22202b
Azis <you@example.com>
2023-12-14 16:11:01 +0600

    form style

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,7 +1,47 @@
 import React from "react";
+import styled from "styled-components";
 
 const Form = () => {
-  return <div>Form</div>;
+  return (
+    <FormStyle>
+      <div>
+        <input type="text" />
+        <input type="date" />
+        <button>ADD</button>
+      </div>
+    </FormStyle>
+  );
 };
+
+const FormStyle = styled.form`
+  display: flex;
+  justify-content: center;
+  margin-top: 60px;
+
+  && div {
+    background-color: #00eeff;
+    display: flex;
+    justify-content: center;
+    padding: 30px;
+    border-radius: 10px;
+    gap: 10px;
+  }
+  && input {
+    padding: 8px;
+    font-size: 18px;
+    border-radius: 10px;
+    border: none;
+  }
+
+  && button {
+    padding: 8px 10px;
+    border: none;
+    border-radius: 10px;
+    font-size: 20px;
+    font-weight: 800;
+    color: white;
+    background-color: orange;
+  }
+`;
 
 export default Form;
