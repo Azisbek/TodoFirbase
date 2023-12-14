ebc5652bddbcc39d75f43010990b97e0907f5c9a
syrttan1 <syrttansyrgakuulu.pi.1.20@gmail.com>
2023-12-14 17:09:25 +0600

    header new ficha

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,29 @@
+import styled from "styled-components"
+
 const Header = ()=>{
     return(
-        <header>Hello</header>
+        <HeaderStyle>
+            <div className="logo"><img src="https://images.unsplash.com/photo-1554080353-a576cf803bda?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8cGhvdG98ZW58MHx8MHx8fDA%3D" alt="" srcset="" /></div>
+            <nav>
+                <ul>
+                    <li>syrttan</li>
+                    <li>amantur</li>
+                    <li>azis</li>
+                    <li>talgat</li>
+                </ul>
+            </nav>
+        </HeaderStyle>
+
     )
 }
+
+const HeaderStyle = styled.header`
+background-color: red;
+
+ul{
+    
+}
+`
+
+
 export default Header
