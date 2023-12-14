64b8cda6d89e7705aec513fefe2169504bf05902
KalykovAJ <aj.kalyk@gmail.com>
2023-12-14 17:13:57 +0600

    List style

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -3,10 +3,38 @@ import styled from 'styled-components'
 
 const List = () => {
   return (
-    <div>
-        list
-    </div>
+    <DivStyle>
+        <ul>
+            <li>
+                <label>name</label>
+                <label>date</label>
+            </li>
+        </ul>
+    </DivStyle>
   )
 }
+
+const DivStyle = styled.div`
+    display: flex;
+    justify-content: center;
+    margin-top: 20px;
+    border-radius: 10px;
+
+  && ul {
+    list-style: none;
+    display: flex;
+    justify-content: space-between;
+    
+  }
+  && li {
+    background-color: #00eeff;
+    padding: 20px;
+    border-radius: 10px;
+    width: 500px;
+    display: flex;
+    justify-content: space-around;
+    }
+
+`
 
 export default List
