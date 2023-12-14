e273e87c0b649451be345905add5453f77e08506
Azis <you@example.com>
2023-12-14 17:03:02 +0600

    form login

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,12 +1,8 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from "react";
+import styled from "styled-components";
 
 const List = () => {
-  return (
-    <div>
-        list
-    </div>
-  )
-}
+  return <div>list</div>;
+};
 
-export default List
+export default List;
